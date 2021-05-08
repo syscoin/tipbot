2b652851fc93a20d85a3e78a5f04b949f84bea62
LucidLunacy <rmathers@live.co.uk>
2021-05-08 09:01:23 +0100

    Created qr.js
    This module is used for returning a user's deposit address as a QR code

---
+++ qr.js
@@ -0,0 +1,42 @@
+var exports = module.exports = {};
+
+const qr = require('qrcode')
+const fs = require('fs-extra')
+
+const db = require('./db.js')
+
+async function checkQRDir() {
+  try {
+    await fs.ensureDir("./qr")
+    console.log('qr directory created')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+checkQRDir()
+
+exports.getQR = async function(userID) {
+  var profile = await db.getProfile(userID)
+
+  var pathToFile = `./qr/${userID}.png`
+  var pathExists = await fs.pathExists(pathToFile)
+
+  if (pathExists) {
+    return pathToFile
+  } else {
+    await qr.toFile(`./qr/${userID}.png`, profile.address, {
+      color: {
+        dark: "#0011a8",
+        light: "#ffffff"
+      }
+    }, function (err) {
+      if (err) {
+        console.log(err)
+        return null
+      }
+    })
+
+    return pathToFile
+  }
+}
