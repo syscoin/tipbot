36878ac4778efebd517b1926f02a552c216ada0f
LucidLunacy <rmathers@live.co.uk>
2021-05-09 17:13:29 +0100

    add catches

--- qr.js
+++ qr.js
@@ -18,19 +18,29 @@ async function checkQRDir() {
 checkQRDir()
 
 async function addLogo(pathToFile, pathToLogoQR) {
-  var image = await sharp(pathToFile)
-    .composite([{ input: './qr/syscoin.png', blend: 'atop'}])
-  await image.toFile(pathToLogoQR)
-  fs.remove(pathToFile)
-  return pathToLogoQR
+  try {
+    var image = await sharp(pathToFile)
+      .composite([{ input: './qr/syscoin.png', blend: 'atop'}])
+    await image.toFile(pathToLogoQR)
+    fs.remove(pathToFile)
+    return pathToLogoQR
+  } catch (error) {
+    console.log("Error adding logo to QR")
+    console.log(error)
+    return null
+  }
 }
 
 exports.getQR = async function(userID) {
-  var profile = await db.getProfile(userID)
+  try {
+    var profile = await db.getProfile(userID)
+  } catch (error) {
+    console.log(error)
+    return
+  }
 
   var pathToFile = `./qr/${userID}qr.png`
   var pathToLogoQR = `./qr/${userID}.png`
-  var pathExists = await fs.pathExists(pathToFile)
 
   var qrFile
   try {
