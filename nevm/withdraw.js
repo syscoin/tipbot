43647fe91a2845a6b21ebc64172c0f667a30d0d4
Ted Ian Osias <osiastedian@gmail.com>
2022-09-02 23:33:00 +0800

    feat(withdrawal): apply erc20 token

--- nevm/withdraw.js
+++ nevm/withdraw.js
@@ -3,6 +3,7 @@ const constants = require("../constants");
 const db = require("../db");
 const utils = require("../utils");
 const config = require("../config.json");
+const { getErc20Contract } = require("./utils/contract");
 const prefix = config.prefix;
 
 const sendUsageExample = (message) => {
@@ -33,6 +34,78 @@ const sendInvalidAmount = (message) => {
 };
 
 /**
+ * @typedef {Object} WithdrawTokenProps
+ * @property {Discord.Message} message Discord message
+ * @property {string} tokenSymbol Symbol of the user
+ * @property {string} userId User id of the message sender
+ * @property {ethers.providers.JsonRpcProvider} jsonRpc Ethers JSON PRC Provider
+ * @property {string} amount amount of withdrawal
+ * @property {ethers.Wallet} wallet nevm
+ * @property {string} recepientAddress NEVM address of Recepient
+ */
+
+/**
+ *
+ * @param {WithdrawTokenProps} params
+ * @returns
+ */
+const generateWithdrawSignedTransaction = async (params) => {
+  const {
+    tokenSymbol,
+    message,
+    jsonRpc,
+    userId,
+    amount,
+    wallet,
+    recepientAddress,
+  } = params;
+  const isWithdrawAll = amount === "all";
+  const token = config.nevm.supportedTokens.find(
+    (token) => token.symbol === tokenSymbol.toUpperCase()
+  );
+  if (!token) {
+    return message.channel.send({
+      embed: {
+        color: constants.FAIL_COL,
+        description: `Hi, **<@${userId}>** \n*${tokenSymbol.toUpperCase()}* is not supported.`,
+      },
+    });
+  }
+  const tokenContract = getErc20Contract(token.address, jsonRpc);
+  const balance = await tokenContract.balanceOf(wallet.address);
+  if (!isWithdrawAll && etherUtils.parseEther(amount).gt(balance)) {
+    message.channel.send({
+      embed: {
+        color: constants.FAIL_COL,
+        description:
+          "Sorry, you cannot withdraw more than is available in your balance.",
+      },
+    });
+    return;
+  }
+
+  let value = isWithdrawAll ? balance : etherUtils.parseEther(amount);
+
+  const nonce = await jsonRpc.getTransactionCount(wallet.address);
+
+  const transferTransactionConfig =
+    await tokenContract.populateTransaction.transfer(recepientAddress, value);
+
+  const transactionConfig = {
+    type: 2,
+    chainId: config.nevm.chainId,
+    value: 0,
+    gasLimit: config.nevm.tokenGasLimit,
+    nonce,
+    maxFeePerGas: etherUtils.parseUnits("10", "gwei"),
+    maxPriorityFeePerGas: etherUtils.parseUnits("2", "gwei"),
+    ...transferTransactionConfig,
+  };
+
+  return wallet.signTransaction(transactionConfig);
+};
+
+/**
  * Withdraw SYS from author's nevm wallet.
  * @param {Discord.Client} client Discord Client
  * @param {Discord.Message} message Discord message
@@ -43,7 +116,7 @@ async function withdraw(client, message, args, jsonRpc) {
   if (args.length < 2) {
     return sendUsageExample(message);
   }
-  const [address, amount] = args;
+  const [address, amount, nevmCommand, tokenSymbol] = args;
   const isWithdrawAll = amount === "all";
 
   if (!etherUtils.isAddress(address)) {
@@ -83,6 +156,7 @@ async function withdraw(client, message, args, jsonRpc) {
   }
 
   const nevmWallet = await db.nevm.getNevmWallet(userId);
+  const wallet = new ethers.Wallet(nevmWallet.privateKey);
 
   if (!nevmWallet) {
     return message.channel
@@ -98,63 +172,76 @@ async function withdraw(client, message, args, jsonRpc) {
       });
   }
 
-  const balance = await jsonRpc.getBalance(nevmWallet.address);
-
-  if (!isWithdrawAll && etherUtils.parseEther(amount).gt(balance)) {
-    message.channel.send({
-      embed: {
-        color: constants.FAIL_COL,
-        description:
-          "Sorry, you cannot withdraw more than is available in your balance.",
-      },
+  let signedTransaction = null;
+  if (tokenSymbol) {
+    signedTransaction = await generateWithdrawSignedTransaction({
+      tokenSymbol,
+      message,
+      jsonRpc,
+      userId,
+      amount,
+      wallet,
+      recepientAddress: address,
     });
-    return;
+  } else {
+    const balance = await jsonRpc.getBalance(nevmWallet.address);
+
+    if (!isWithdrawAll && etherUtils.parseEther(amount).gt(balance)) {
+      message.channel.send({
+        embed: {
+          color: constants.FAIL_COL,
+          description:
+            "Sorry, you cannot withdraw more than is available in your balance.",
+        },
+      });
+      return;
+    }
+
+    const gasLimit = config.nevm.gasLimit;
+    const maxFeePerGas = etherUtils.parseUnits("10", "gwei");
+    const maxGasFee = maxFeePerGas.mul(gasLimit);
+    let value = isWithdrawAll
+      ? balance.sub(maxGasFee)
+      : etherUtils.parseEther(amount);
+
+    if (!value.gt(0)) {
+      message.channel.send({
+        embed: {
+          color: constants.FAIL_COL,
+          description:
+            "The value you are trying to withdraw must be a valid number more than 0.",
+        },
+      });
+      return;
+    }
+
+    const nonce = await jsonRpc.getTransactionCount(wallet.address);
+    const minTip = etherUtils.parseUnits(`${config.tipMin}`, "ether");
+    const minimumAmount = minTip.add(maxGasFee);
+
+    if (!isWithdrawAll && value.lt(minTip.add(maxGasFee))) {
+      return message.channel.send({
+        embed: {
+          color: constants.FAIL_COL,
+          description: `The value you are trying to withdraw is too small, it must be more than ${etherUtils.formatEther(
+            minimumAmount.toString()
+          )}.`,
+        },
+      });
+    }
+
+    signedTransaction = await wallet.signTransaction({
+      type: 2,
+      chainId: config.nevm.chainId,
+      to: address,
+      value,
+      gasLimit,
+      nonce,
+      maxFeePerGas,
+      maxPriorityFeePerGas: etherUtils.parseUnits("2", "gwei"),
+    });
   }
 
-  const gasLimit = config.nevm.gasLimit;
-  const maxFeePerGas = etherUtils.parseUnits("10", "gwei");
-  const maxGasFee = maxFeePerGas.mul(gasLimit);
-  let value = isWithdrawAll
-    ? balance.sub(maxGasFee)
-    : etherUtils.parseEther(amount);
-
-  if (!value.gt(0)) {
-    message.channel.send({
-      embed: {
-        color: constants.FAIL_COL,
-        description:
-          "The value you are trying to withdraw must be a valid number more than 0.",
-      },
-    });
-    return;
-  }
-
-  const wallet = new ethers.Wallet(nevmWallet.privateKey);
-  const nonce = await jsonRpc.getTransactionCount(wallet.address);
-  const minTip = etherUtils.parseUnits(`${config.tipMin}`, "ether");
-  const minimumAmount = minTip.add(maxGasFee);
-
-  if (!isWithdrawAll && value.lt(minTip.add(maxGasFee))) {
-    return message.channel.send({
-      embed: {
-        color: constants.FAIL_COL,
-        description: `The value you are trying to withdraw is too small, it must be more than ${etherUtils.formatEther(
-          minimumAmount.toString()
-        )}.`,
-      },
-    });
-  }
-
-  const signedTransaction = await wallet.signTransaction({
-    type: 2,
-    chainId: config.nevm.chainId,
-    to: address,
-    value,
-    gasLimit,
-    nonce,
-    maxFeePerGas,
-    maxPriorityFeePerGas: etherUtils.parseUnits("2", "gwei"),
-  });
   console.log("Sending Transaction...", wallet.address);
   jsonRpc
     .sendTransaction(signedTransaction)
